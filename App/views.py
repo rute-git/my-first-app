ed3067440003be0224a5d17bd3b878a7c17cdf14
rute-git <chemaneakiara@gmail.com>
2025-06-17 19:26:40 +0200

    Changed the HTML for the site.

--- App/views.py
+++ App/views.py
@@ -5,4 +5,4 @@ from .models import Post
 def post_list(request):
     Posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 
-    return render(request, 'App/post_list.html',{'Posts':Posts})
+    return render(request, 'App/post_list.html', {'Posts':Posts})
